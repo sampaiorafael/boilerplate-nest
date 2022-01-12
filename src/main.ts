47cbb25f10237aee809167cc608d698628276d53
sampaiorafael <rafasampaio021@hotmail.com>
2022-01-12 16:01:25 -0300

    feat: swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import SwaggerConfig from './configs/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  SwaggerConfig.init(app);
+
   await app.listen(3000);
 }
 bootstrap();
